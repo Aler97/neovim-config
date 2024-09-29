5a89503d061a80cc3a9787b6fcd3b5e000e79fb0
Aleksandar <radicevicsasa836@gmail.com>
2024-09-29 12:43:21 +0200

    prettier autoformat

--- init.lua
+++ init.lua
@@ -581,15 +581,17 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes =
-					{ c = true, cpp = true, javascript = true, typescript = true, html = true, astro = true }
+				local disable_filetypes = { c = true, cpp = true }
 				return {
-					timeout_ms = 500,
+					timeout_ms = 5000,
 					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
 				}
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				html = { "prettier" },
+				javascript = { "prettier" },
+				typescript = { "prettier" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
